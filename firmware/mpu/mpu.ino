37e9c3cdbc5f5edf7e909521d0167d4ecf0bae30
shanebotclub <shaned75@googlemail.com>
2026-05-11 23:16:30 +0100

    testing

--- firmware/mpu/mpu.ino
+++ firmware/mpu/mpu.ino
@@ -1,28 +1,30 @@
 #include <Wire.h>
-#include <MPU6050_light.h>
-
-MPU6050* mpu;   // pointer, not object
 
 void setup() {
   Serial.begin(115200);
   delay(2000);
-  Serial.println("SETUP START");
-
+  Serial.println("I2C Scanner starting...");
   Wire.begin(21, 22);
-  Serial.println("I2C STARTED");
-
-  mpu = new MPU6050(Wire);   // create AFTER I2C is ready
-  Serial.println("MPU object created");
-
-  Serial.println("Calling mpu->begin()...");
-  byte status = mpu->begin();
-  Serial.print("mpu->begin() returned: ");
-  Serial.println(status);
-
-  Serial.println("SETUP DONE");
 }
 
 void loop() {
-  Serial.println("LOOP");
-  delay(1000);
+  byte error, address;
+  int nDevices = 0;
+
+  Serial.println("Scanning...");
+
+  for (address = 1; address < 127; address++) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+
+    if (error == 0) {
+      Serial.print("I2C device found at 0x");
+      Serial.println(address, HEX);
+      nDevices++;
+    }
+  }
+
+  if (nDevices == 0) Serial.println("No I2C devices found");
+  Serial.println();
+  delay(2000);
 }
